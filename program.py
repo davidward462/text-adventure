76fe6d37a871ed67e3e1d3202dde6de231e3110c
David Ward <davidward462@users.noreply.github.com>
2025-08-20 21:18:23 -0700

    add more Node attributes, remove test objects

--- program.py
+++ program.py
@@ -1,18 +1,24 @@
 
 # 'children' is a list of objects that are either representationally inside this node, or can be reached from it, or are a subsection of it.
 # 'tags' is a list of strings that are used to identify the object.
-# 'description' is a more verbose description of the object the player can see.
+# 'description' is a short description of the object the player can see.
+# 'details' is text that the player sees if they examine the object closely.
 # 'weight' relates to how easy an item is to pick up.
 # 'destination' is only for passages, and is the place to where it leads.
+# 'prospect' is the place to which the passage appears to go.
 # 'health' is the number of hit points for entities
+# 'goText' is text the player sees when passing through this passage.
 class Node():
-        def __init__(self, tags, description, weight=None, destination=None, health=None):
+        def __init__(self, tags, description, details="You see nothing special.", weight=None, destination=None, prospect=None, health=None, goText=None):
                 self.children = []
                 self.tags = tags
                 self.description = description
+                self.details = details
                 self.weight = weight
                 self.destination = destination
+                self.prospect = prospect
                 self.health = health
+                self.goText = goText
 
         def addChild(self, childNode):
                 self.children.append(childNode)
@@ -111,32 +117,6 @@ def parseInput(text, player):
                 case _:
                         print(f"You don't know how to do '{verb}'")
         return True
-
-# Player <- (tags=[...], description="descr", location=loc)
-# Location <- (tags=[...], description="descr")
-# Passage <- (tags=[...], description="descr", destination=dest)
-
-root = Node(["world"], "the game world")
-
-field = Node(["field"], "a grassy field")
-player = Player(["player", "me", "yourself"], "a hero", field)
-cow = Node(["brown", "cow"], "a brown cow")
-tree = Node(["birch", "tree"], "a birch tree")
-house = Node(["house", "north"], "a blue house")
-house.makeEnterable()
-
-cave = Node(["cave", "east"], "a small cave")
-caveExit = Node(["exit"], "an exit from the cave", destination=field)
-cave.makeEnterable()
-
-bed = Node(["bed"], "a ragged bed")
-stove = Node(["stove"], "an old, cast-iron stove")
-door = Node(["door"], "a wooden door", destination=field)
-
-root.addChild(field)
-field.addChildren([cow, tree, house, cave, player])
-house.addChildren([bed, stove, door])
-cave.addChild(caveExit)
 
 # main loop
 running = True

fb5e6c7113b695788be8bc8ed24e2b3e0fe92eb4
David Ward <davidward462@users.noreply.github.com>
2025-08-20 21:32:15 -0700

    create new testing objects

--- program.py
+++ program.py
@@ -1,40 +1,4 @@
-
-# 'children' is a list of objects that are either representationally inside this node, or can be reached from it, or are a subsection of it.
-# 'tags' is a list of strings that are used to identify the object.
-# 'description' is a short description of the object the player can see.
-# 'details' is text that the player sees if they examine the object closely.
-# 'weight' relates to how easy an item is to pick up.
-# 'destination' is only for passages, and is the place to where it leads.
-# 'prospect' is the place to which the passage appears to go.
-# 'health' is the number of hit points for entities
-# 'goText' is text the player sees when passing through this passage.
-class Node():
-        def __init__(self, tags, description, details="You see nothing special.", weight=None, destination=None, prospect=None, health=None, goText=None):
-                self.children = []
-                self.tags = tags
-                self.description = description
-                self.details = details
-                self.weight = weight
-                self.destination = destination
-                self.prospect = prospect
-                self.health = health
-                self.goText = goText
-
-        def addChild(self, childNode):
-                self.children.append(childNode)
-
-        def addChildren(self, childrenList):
-                for child in childrenList:
-                        self.addChild(child)
-
-        def makeEnterable(self):
-                self.destination = self
-
-class Player(Node):
-        def __init__(self, tags, description, location, weight=None, destination=None, health=100):
-                self.inventory = []
-                self.location = location
-                Node.__init__(self, tags, description, weight, destination, health)
+from node import Node, Player
 
 
 # return the object specified by the tags, in the node. Return None if it is not found.
@@ -118,8 +82,18 @@ def parseInput(text, player):
                         print(f"You don't know how to do '{verb}'")
         return True
 
+root = Node(["root"], "")
+house = Node(["house"], "a small wooden house")
+player = Player(["player"], "a hero", location=house)
+field = Node(["field"], "a grassy field")
+door = Node(["door"], "a wooden door", destination=field)
+
+root.addChild(field)
+field.addChild(house)
+house.addChildren([player, door])
+
 # main loop
-running = False
+running = True
 while running:
         text = input("\n-> ")
         running = parseInput(text, player)

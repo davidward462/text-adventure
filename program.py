79a2e7c921c5b5bbfd8c0c0955d6de577c8e607c
David Ward <davidward462@users.noreply.github.com>
2025-08-22 14:03:10 -0700

    add basement and stairs

--- program.py
+++ program.py
@@ -2,15 +2,23 @@ from node import Node, Player
 import lib
 
 root = Node(["root"], "")
-house = Node(["house"], "a small wooden house")
+house = Node(["house"], "a small wooden house", goText="You enter the house.")
 house.makeEnterable()
 player = Player(["player"], "a hero", location=house)
 field = Node(["field"], "a grassy field")
-door = Node(["door"], "a wooden door", destination=field)
+door = Node(["door"], "a wooden door", destination=field, goText="You go through the door.")
+basement = Node(["basement"], "a dimly-lit basement")
+staircaseDown = Node(["stairs", "staircase", "down", "basement"], "a staircase leading down to the basement", destination=basement, goText="You descend the stairs.")
+staircaseUp = Node(["stairs", "staircase", "up"], "a staircase leading up", destination=house, goText="You ascend the stairs.")
+oldBook = Node(["book"], "an old book", weight=1)
 
-root.addChild(field)
+root.addChildren([field, basement])
 field.addChild(house)
-house.addChildren([player, door])
+house.addChildren([player, door, staircaseDown])
+basement.addChildren([staircaseUp, oldBook])
+
+
+lib.executeLook("around", player)
 
 # main loop
 running = True
